1aa97646fee9c4053795804d92a509d2f55ce61f
ElthonD <elthon.rivas@gmail.com>
2023-11-09 15:11:51 -0600

    cvcv

--- app.py
+++ app.py
@@ -181,7 +181,7 @@ def g_recuperacion(df):
                         text= [f'Recuperados(s): {x:.0f}%' for x in df['Recuperados (%)']])
     
     # Create a layout with interactive elements and two yaxes
-    layout = go.Layout(height=500, width=500, font=dict(size=10),
+    layout = go.Layout(height=500, width=100, font=dict(size=10),
                    title='Robos',
                    plot_bgcolor="#FFF",
                    xaxis=dict(showgrid=False, title='Fecha',
