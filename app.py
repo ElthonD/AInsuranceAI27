3f9bacd1748232317df438f9231e721a2b164edb
ElthonD <elthon.rivas@gmail.com>
2023-11-09 14:44:14 -0600

    dfdfdf

--- app.py
+++ app.py
@@ -244,9 +244,10 @@ try:
     st.markdown("<h3 style='text-align: left;'>Indicadores</h3>", unsafe_allow_html=True)
 
     df11 = df.groupby(['Estatus']).size()
-    df11.set_axis(['Estatus', 'Total'], axis=1)
+    Frec = pd.DataFrame(df11)
+    #df11.set_axis(['Estatus', 'Total'], axis=1)
     #df11.rename(columns={0:'Total'})
-    st.dataframe(df11)
+    st.dataframe(Frec)
     
     c1, c2 = st.columns((1,1))
     with c1:
