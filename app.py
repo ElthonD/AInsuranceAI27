c96c5be7bbd83a20d3081418c8f7e1214d940dc8
ElthonD <elthon.rivas@gmail.com>
2023-11-09 15:16:33 -0600

    sdfsdf

--- app.py
+++ app.py
@@ -141,7 +141,8 @@ def df_grafico(df):
         df5 = df5.reset_index()
 
         # Preparar Dataframe Final
-        df5['Mes'] = df5['Fecha y Hora'].dt.month_name(locale='Spanish')
+        df5['MesN'] = df5['Fecha y Hora'].apply(lambda x: x.month)
+        df5['Mes'] = df5['MesN'].map({1:"Enero", 2:"Febrero", 3:"Marzo", 4:"Abril", 5:"Mayo", 6:"Junio", 7:"Julio", 8:"Agosto", 9:"Septiembre", 10:"Octubre", 11:"Noviembre", 12:"Diciembre"})
         df5['Año'] = df5['Fecha y Hora'].dt.year
         df5 = df5.fillna(0)
         df5['Total'] = (df5['RECUPERADO'] + df5['CONSUMADO'])
