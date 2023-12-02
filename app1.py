95e3eb0b512eed20ab66b47ca45b72c734120c47
ElthonD <elthon.rivas@gmail.com>
2023-12-02 17:56:33 -0600

    pruebas datetime

--- app1.py
+++ app1.py
@@ -123,7 +123,8 @@ if authentication_status:
     elif options=="Data Visualización":
         
         df = pd.DataFrame(obtener_df())
-        df['Fecha'] = pd.to_datetime(df['Fecha'], format='%d-%m-%Y', errors='coerce')
+        df['Fecha'].strftime('%Y-%m-%d')
+        #df['Fecha'] = pd.to_datetime(df['Fecha'], format='%d-%m-%Y', errors='coerce')
         st.dataframe(df)
         print(df.dtypes)
         df = df[['Fecha', 'Nombre Monitorista', 'Bitácora', 'Cliente', 'Motivo de Entrada', 'Marca', 'Modelo', 'Placas', 'Economico', 'Latitud', 'Longitud', 'Estado', 'Municipio', 'Tramo', 'Estatus', 'Observaciones']]
