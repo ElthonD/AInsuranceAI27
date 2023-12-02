edee1e6023689101daf0d786fb546cc1869b669d
ElthonD <elthon.rivas@gmail.com>
2023-12-02 09:51:13 -0600

    cambios data visual

--- app1.py
+++ app1.py
@@ -126,7 +126,10 @@ if authentication_status:
 
     elif options=="Data Visualización":
         
-        print(obtener_df())
+        dict_df = obtener_df()
+        df2 = pd.DataFrame([[key, dict_df[key]] for key in dict_df.keys()], columns=['Fecha', 'Nombre Monitorista', 'Bitácora', 'Cliente', 'Motivo de Entrada', 'Marca', 'Modelo', 'Placas', 'Económico', 'Latitud', 'Longitud', 'Estado', 'Municipio', 'Tramo', 'Estatus', 'Observaciones'])
+        st.dataframe(df2)
+
 
         #df = pd.DataFrame(get_all_periods())
         #df['Fecha'] = pd.to_datetime(df['Fecha'], format='%Y-%m-%d', errors='coerce')
