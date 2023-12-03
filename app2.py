a2030f5e64fa1e3a9ee9e417aee1273ff3cf0776
ElthonD <elthon.rivas@gmail.com>
2023-12-03 10:44:52 -0600

    actualizar

--- app2.py
+++ app2.py
@@ -42,8 +42,8 @@ def obtener_df():
     data = ainsurance_db.fetch_all_ainsurance()
     return data
 
-def editar_df():
-    data = ainsurance_db.update_ainsurance()
+def editar_df(fecha, ndocumentador, nBitacora, sCliente, mEntrada, marca, modelo, placas, economico, latitud, longitud, estado, municipio, tramo, estatus, coment, updates):
+    data = ainsurance_db.update_ainsurance(fecha, ndocumentador, nBitacora, sCliente, mEntrada, marca, modelo, placas, economico, latitud, longitud, estado, municipio, tramo, estatus, coment, updates)
     return data
     
 col4, col5, col6 = st.columns([1,1,1])
