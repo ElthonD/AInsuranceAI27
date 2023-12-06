939e828b6c3e47f6bfbea3668354e74a7044358a
ElthonD <elthon.rivas@gmail.com>
2023-12-06 16:46:03 -0600

    ajustes

--- app3.py
+++ app3.py
@@ -282,7 +282,7 @@ names = [user["name"] for user in users]
 hashed_passwords = [user["password"] for user in users]
 
 #authenticator = stauth.Authenticate(names, usernames, hashed_passwords, "ai27_ainsurance", "abcdef", cookie_expiry_days=30)
-authenticator = stauth.Authenticate(names, usernames, hashed_passwords)
+authenticator = stauth.Authenticate(names, usernames, hashed_passwords, "ai27_ainsurance", "abcdef")
 
 # --- DATABASE INTERFACE ---
 
